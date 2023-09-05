1cc32e5fcffe0dd44199ca857afb8bcb367c1abe
wenyuhong <wenyuhong@hust.edu.cn>
2023-09-05 21:12:28 +0800

    update run-vm.sh

--- run-vm.sh
+++ run-vm.sh
@@ -7,10 +7,11 @@ usage() {
     exit
 }
 
-IODA_IMGDIR="./images/"
+IODA_IMGDIR="./images"
 # IODA_IMGDIR="/home/wenyuhong/vdo/"
 IODA_KERNEL="src/iodaLinux/arch/x86/boot/bzImage"
 IODA_FEMU="src/iodaFEMU/build-femu/x86_64-softmmu/qemu-system-x86_64"
+Share_Dir_Path="./share"
 
 sudo sh -c 'echo 1 > /proc/sys/vm/drop_caches'
 echo 2 | sudo tee /sys/kernel/mm/ksm/run >/dev/null 2>&1
@@ -24,12 +25,12 @@ sleep 3
 MB=1
 let GB=1024*${MB}
 device_count=5
-let array_size=512*${GB}
-let device_size_per=${array_size}/${device_count}
+let array_size_MB=512*${GB}
+let device_size_MB_per=${array_size_MB}/${device_count}
 
 device_command=""
 for((i=1;i<=${device_count};i++));
-do device_command+="-device femu,devsz_mb=${device_size_per},femu_mode=1 "; done
+do device_command+="-device femu,devsz_mb=${device_size_MB_per},femu_mode=1 "; done
 
 sudo ${IODA_FEMU} \
     -name "iodaVM" \
@@ -38,7 +39,7 @@ sudo ${IODA_FEMU} \
     -m 40G \
     -enable-kvm \
     -boot menu=on \
-    -fsdev local,security_model=passthrough,id=fsdev0,path=/home/wenyuhong/share \
+    -fsdev local,security_model=passthrough,id=fsdev0,path=${Share_Dir_Path} \
     -device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare \
     -drive file=${IODA_IMGDIR}/u20s.qcow2,if=virtio,cache=none,aio=native,format=qcow2 \
     ${device_command} \
@@ -54,7 +55,7 @@ echo "femu finish!"
 # read_ratio=0            #0, 50, 100
 # access_pattern="rand"   #seq, rand, skew
 
-Dedup_engine="LightDedup"  #-Dmdedup -LightDedup -DedupSSD
+Dedup_engine="Dmdedup"  #-Dmdedup -LightDedup -DedupSSD
 
 # if [ $Dedup_engine == "Dmdedup" ]
 # then
@@ -71,8 +72,8 @@ Dedup_engine="LightDedup"  #-Dmdedup -LightDedup -DedupSSD
 # GC_threshould=5
 # R_MapTable=100
 
-# root_name="vdbench(split_lba)(warm_dedup_50G)"
-root_name="Workloads_res"
+# root_name="vdbench"
+root_name="Workloads_res_raid5"
 # dir_name="configurations/${cache_size}cache-${duplicate_ratio}%Dedup-Per${transaction}-r${read_ratio}-${access_pattern}"
 # dir_name="R_MapTable/${duplicate_ratio}%Dedup-${R_MapTable}%R_MapTable"
 # dir_name="new_R_MapTable/${duplicate_ratio}%Dedup-${R_MapTable}%R_MapTable"
@@ -80,7 +81,7 @@ root_name="Workloads_res"
 # dir_name="GC_threshould/${GC_threshould}%GC"
 # dir_name="SSDs_of_array"
 # Dedup_engine="${device_count}-SSDs-${Dedup_engine}"
-# dir_name="Mail-3-IOx2"
+# dir_name="temp"
 dir_name="Homes"
 # dir_name="test/${cache_size}cache-${duplicate_ratio}%Dedup-Per${transaction}-r${read_ratio}-${access_pattern}"
 
@@ -91,19 +92,19 @@ mkdir -p "./$root_name/$dir_name/$Dedup_engine"
 cp ./SSDInfo_*.log "./$root_name/$dir_name/$Dedup_engine/"
 # cp ./SSDLatency_*.log ./$root_name/$dir_name/$Dedup_engine/
 cp ./ioda-femu.log "./$root_name/$dir_name/$Dedup_engine/"
-cp /home/wenyuhong/share/dedup_info.log "./$root_name/$dir_name/$Dedup_engine/"
-cp /home/wenyuhong/share/result_IOPS.log "./$root_name/$dir_name/$Dedup_engine/"
-# cp /home/wenyuhong/share/running_res.log ./$root_name/$dir_name/$Dedup_engine/
-# cp /home/wenyuhong/share/xremap-run-dedup.sh "./$root_name/$dir_name/$Dedup_engine/"
-# cp /home/wenyuhong/share/my_run.sh ./$root_name/$dir_name/$Dedup_engine/
-# cp /home/wenyuhong/share/latency-*.log ./$root_name/$dir_name/$Dedup_engine/
-# cp -r /home/wenyuhong/share/output "./$root_name/$dir_name/$Dedup_engine/"
-# cp -r /home/wenyuhong/share/output-warm "./$root_name/$dir_name/$Dedup_engine/"
-cp /home/wenyuhong/share/run_replaytrace.sh "./$root_name/$dir_name/$Dedup_engine/"
-cp /home/wenyuhong/share/run_replaytrace.log "./$root_name/$dir_name/$Dedup_engine/"
-cp /home/wenyuhong/share/replay_trace.log "./$root_name/$dir_name/$Dedup_engine/"
-# cp -r /home/wenyuhong/share/output-warm-SSD "./$root_name/$dir_name/$Dedup_engine/"
-# cp -r /home/wenyuhong/share/output-warm-Dedup "./$root_name/$dir_name/$Dedup_engine/"
+cp ${Share_Dir_Path}/dedup_info.log "./$root_name/$dir_name/$Dedup_engine/"
+cp ${Share_Dir_Path}/result_IOPS.log "./$root_name/$dir_name/$Dedup_engine/"
+# cp ${Share_Dir_Path}/running_res.log ./$root_name/$dir_name/$Dedup_engine/
+# cp ${Share_Dir_Path}/xremap-run-dedup.sh "./$root_name/$dir_name/$Dedup_engine/"
+# cp ${Share_Dir_Path}/my_run.sh ./$root_name/$dir_name/$Dedup_engine/
+# cp ${Share_Dir_Path}/latency-*.log ./$root_name/$dir_name/$Dedup_engine/
+# cp -r ${Share_Dir_Path}/output "./$root_name/$dir_name/$Dedup_engine/"
+# cp -r ${Share_Dir_Path}/output-warm "./$root_name/$dir_name/$Dedup_engine/"
+cp ${Share_Dir_Path}/run_replaytrace.sh "./$root_name/$dir_name/$Dedup_engine/"
+cp ${Share_Dir_Path}/run_replaytrace.log "./$root_name/$dir_name/$Dedup_engine/"
+cp ${Share_Dir_Path}/replay_trace.log "./$root_name/$dir_name/$Dedup_engine/"
+# cp -r ${Share_Dir_Path}/output-warm-SSD "./$root_name/$dir_name/$Dedup_engine/"
+# cp -r ${Share_Dir_Path}/output-warm-Dedup "./$root_name/$dir_name/$Dedup_engine/"
 echo "result files copy finish!"
 
 # rm /home/wenyuhong/git/IODA-SOSP21-AE/SSDInfo_*.log 
